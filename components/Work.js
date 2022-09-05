d0b86161efec7c4afa93e1d97b95b3c5893d397d
AdemCanCertel <110599327+AdemCanCertel@users.noreply.github.com>
2022-09-05 20:02:18 +0300

    Update Work.js

--- components/Work.js
+++ components/Work.js
@@ -5,7 +5,7 @@ export default function Work() {
                 <h1>Featured Projects</h1>
             </div>
             <div className="mt-4 flex gap-6 flex-col md:flex-row">
-              <a href="https://github.com/AdemCanCertel/github-finder" target="_blank" className="w-full md:w-1/3 p-[3px] transition hover:scale-[1.01] transition-all rounded-md bg-gradient-to-r from-yellow-200 via-red-300 to-red-400">
+              <a href="https://github.com/AdemCanCertel/github-finder" target="_blank" className="w-full md:w-1/3 p-[3px] hover:scale-[1.01] transition-all rounded-md bg-gradient-to-r from-gray-500 to-gray-600">
                 <div className="bg-white dark:bg-background_color text-white rounded-md">
                     <h4 className="mb-6 p-2 sm:mb-10 w-full text-lg md:text-lg text-gray-900 dark:text-gray-100 font-medium">GitHub Finder</h4>
                     <div className="p-2 dark:text-white text-black">
@@ -13,7 +13,7 @@ export default function Work() {
                     </div>
                 </div>
               </a>
-              <a href="https://github.com/AdemCanCertel/homepage" target="_blank" className="w-full md:w-1/3 p-[3px] transition hover:scale-[1.01] transition-all rounded-md bg-gradient-to-r from-fuchsia-300 to-red-400">
+              <a href="https://github.com/AdemCanCertel/homepage" target="_blank" className="w-full md:w-1/3 p-[3px] hover:scale-[1.01] transition-all rounded-md bg-gradient-to-r from-gray-500 to-gray-600">
                 <div className="bg-white dark:bg-background_color text-white rounded-md">
                     <h4 className="mb-6 p-2 sm:mb-10 w-full text-lg md:text-lg text-gray-900 dark:text-gray-100 font-medium">Homepage</h4>
                     <div className="p-2 dark:text-white text-black">
